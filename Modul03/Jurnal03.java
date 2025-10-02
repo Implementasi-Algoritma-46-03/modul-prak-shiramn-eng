bcb73b958f3f00181b8debc2ee4422df0c4c9951
Alif Siregar <alifsiregar2007@gmail.com>
2025-10-02 17:50:54 +0700

    update tugas jurnal 3

--- Modul03/Jurnal03.java
+++ Modul03/Jurnal03.java
@@ -10,9 +10,9 @@ public class Jurnal03 {
         int niakhir = tugas3.nextInt();
 
         double jumlah = niprak * 0.25 + niases * 0.35 + niakhir * 0.40;
-        System.out.printf("%.2f", jumlah);
+        System.out.printf("%.2f\n", jumlah);
         boolean nilaiakhir = jumlah > 75;
-        System.out.println(" Lulus MK: " + nilaiakhir);
+        System.out.println("Lulus MK: " + nilaiakhir);
 
         tugas3.close();
     }
