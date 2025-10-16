c52bd27287d74c47e1f2d603ac810c8630c9c14f
shiramn-eng <shifanuraiha@gmail.com>
2025-10-16 22:14:17 +0700

    memperbaiki

--- Modul05/Jurnal02.java
+++ Modul05/Jurnal02.java
@@ -1,31 +1,66 @@
 import java.util.Scanner;
 
 public class Jurnal02 {
-    public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);
-        String hariIni = s.nextLine();
-        int hariEsok = s.nextInt();
-        int hariKe = 0;
 
-        switch (hariIni){
-            case "Senin": hariKe = 0;break;
-            case "Selasa": hariKe = 1;break;
-            case "Rabu": hariKe = 2;break;
-            case "Kmais": hariKe = 3;break;
-            case "Jum'at": hariKe = 4;break;
-            case "Sabtu": hariKe = 5;break;
-            case "Minggu": hariKe = 6;break;
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+        String namaHari = input.nextLine();
+        int bilangan = input.nextInt();
+        int hari = 0;
+
+        switch (namaHari) {
+            case "Senin":
+            hari = 0;
+        break;
+            case "Selasa":
+            hari = 1;
+        break;
+            case "Rabu":
+            hari = 2;
+        break;
+            case "Kamis":
+            hari = 3;
+        break;
+            case "Jumat":
+            hari = 4;
+        break;
+            case "Sabtu":
+            hari = 5;
+        break;
+            case "Minggu":
+            hari = 6;
+        break;
+        default:
+        System.out.println("Nama Hari Tidak Valid");
         }
-        int totalHari = (hariKe + (hariEsok % 7)) % 7;
-        
-        switch (totalHari){
-            case 0: System.out.println("Senin");break;
-            case 1: System.out.println("Selasa");break;
-            case 2: System.out.println("Rabu");break;
-            case 3: System.out.println("Kamis");break;
-            case 4: System.out.println("Jum'at");break;
-            case 5: System.out.println("Sabtu");break;
-            case 6: System.out.println("Minggu");break;
+
+        int hasilIndex = (hari + bilangan) % 7;
+
+        String hasilHari = "";
+
+        switch (hasilIndex) {
+            case 0:
+            hasilHari = "Senin";
+        break;
+            case 1:
+            hasilHari = "Selasa";
+        break;
+            case 2:
+            hasilHari = "Rabu";
+        break;
+            case 3:
+            hasilHari = "Kamis";
+        break;
+            case 4:
+            hasilHari = "Jumat";
+        break;
+            case 5:
+            hasilHari = "Sabtu";
+        break;
+            case 6:
+            hasilHari = "Minggu";
+        break;
         }
+        System.out.println(hasilHari);
     }
 }
