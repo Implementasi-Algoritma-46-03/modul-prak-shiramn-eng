6ee145fe21b96a30feddbc787ea6937d624cedde
shiramn-eng <shifanuraiha@gmail.com>
2025-10-23 18:09:32 +0700

    masukan jurnal 1,2,3

--- Modul06/Jurnal02.java
+++ Modul06/Jurnal02.java
@@ -1,6 +1,61 @@
+import java.util.Scanner;
+
 public class Jurnal02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+        String lagu = input.nextLine();
+
+        System.out.println(vokal2(vokal1(lagu)));
+    }
+
+    private static String vokal1(String lagu) {
+        int sisaBagi = lagu.length() % 5;
+        String hasil = null; 
+
+        switch (sisaBagi) {
+            case 0:
+                hasil = lagu.replaceAll("[aiueo]", "a");
+                break;
+            case 1:
+                hasil = lagu.replaceAll("[aiueo]", "e");
+                break;
+            case 2:
+                hasil = lagu.replaceAll("[aiueo]", "i");
+                break;
+            case 3:
+                hasil = lagu.replaceAll("[aiueo]", "o");
+                break;
+            case 4:
+                hasil = lagu.replaceAll("[aiueo]", "u");
+                break;
+        }
+
+        return hasil;
+    }
+
+    private static String vokal2(String vokal1) {
+        int sisaBagi = vokal1.length() % 5;
+        String hasil = null; 
+
+        switch (sisaBagi) {
+            case 0:
+                hasil = vokal1.replaceAll("[AIUEO]", "A");
+                break;
+            case 1:
+                hasil = vokal1.replaceAll("[AIUEO]", "E");
+                break;
+            case 2:
+                hasil = vokal1.replaceAll("[AIUEO]", "I");
+                break;
+            case 3:
+                hasil = vokal1.replaceAll("[AIUEO]", "O");
+                break;
+            case 4:
+                hasil = vokal1.replaceAll("[AIUEO]", "U");
+                break;
+        }
+
+        return hasil;
     }
 }
--- Modul06/Jurnal03.java
+++ Modul06/Jurnal03.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Jurnal03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+     Scanner input = new Scanner(System.in);
+        double r = input.nextDouble();
+        double h = input.nextDouble();
+        double t = input.nextDouble();
+
+        // Panggil method untuk menghitung
+        double volumeMaks = hitungVolume(r, h);
+        double volumeAir = hitungVolume(r, t);
+        double persen = hitungPersen(t, h);
+
+        System.out.printf("%.2f %.2f %.1f%%", volumeMaks, volumeAir, persen);
+    }
+    private static double hitungVolume(double r, double tinggi) {
+        return Math.PI * r * r * tinggi;
+    }
+    private static double hitungPersen(double t, double h) {
+        return (t / h) * 100;
     }
 }
